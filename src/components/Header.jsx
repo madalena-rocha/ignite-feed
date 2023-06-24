7f546ccd00a338ffd9776da8c806152614f2ce3b
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-06-23 23:20:12 -0300

    finish component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 // Ao utilizar CSS Modules, para importar um arquivo é necessário nomear os estilos
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
