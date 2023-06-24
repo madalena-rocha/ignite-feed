01880e5cc9a332813d3a2a4e0750376ef5cd0ae6
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-06-23 22:36:09 -0300

    :tada: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// O react é o pacote responsável pelo core do React, contendo as funcionalidades compartilhadas entre 
+// todos os ambientes, seja React Native, React para Web, TV, VR...
+import ReactDOM from 'react-dom/client'
+// O react-dom é a integração do react com a DOM, que é a representação do HTML através do JavaScript, 
+// ou seja, ao importar o react-dom, estamos integrando o rect para funcionar no ambiente web, no browser
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
