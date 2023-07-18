bd0514f080b5b23ae0de2b2ca033294550abe26b
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-07-18 15:45:52 -0300

    add comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,7 +21,7 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
+        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
         <p>
           <a href="">#novoprojeto</a>{' '}
           {/* {' '} para adicionar espaço em branco */}
@@ -29,6 +29,18 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
