0567b3327fe80e0997a5bcd1adc11bfd35743395
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-07-18 15:12:38 -0300

    finish component Post

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // JSX = JavaScript + XML (HTML)
 
 import { Header } from './components/Header.jsx';
-import { Post } from './Post.jsx';
+import { Post } from './components/Post.jsx';
 import { Sidebar } from './components/Sidebar.jsx';
 
 import styles from './App.module.css';
---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/madalena-rocha.png" />
+
+          <div className={styles.authorInfo}>
+            <strong>Madalena Rocha</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        {/* O atributo dateTime mostra a data da publicação na inspeção do HTML 
+        O atributo title mostra a data da publicação quando o usuário passa o mouse em cima do time */}
+        <time title="07 de Julho às 14:30h" dateTime="2023-18-07 14:30:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          {/* {' '} para adicionar espaço em branco */}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
